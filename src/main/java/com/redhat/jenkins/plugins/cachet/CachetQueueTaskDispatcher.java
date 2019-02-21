47a9c514198929084687dbb0bcff7aefac347bd8
Scott Hebert <shebert@redhat.com>
2019-02-21 11:41:28 -0500

    Add missing Operational status to metrics

--- src/main/java/com/redhat/jenkins/plugins/cachet/CachetQueueTaskDispatcher.java
+++ src/main/java/com/redhat/jenkins/plugins/cachet/CachetQueueTaskDispatcher.java
@@ -68,7 +68,7 @@ public class CachetQueueTaskDispatcher extends QueueTaskDispatcher {
                         if (!watchedResources.containsKey(name)) {
                             msgs.add(String.format("%s: %s", name, "Unknown resource"));
                             requiredResourcesMetric.setGateUpdatedTime(System.currentTimeMillis());
-                            requiredResourcesMetric.setGatingStatus("Unknown resource");
+                            requiredResourcesMetric.setGatingStatus(ResourceStatus.UNKNOWN);
                         } else {
                             Resource r = watchedResources.get(name);
                             if (r.getStatusId() != ResourceStatus.OPERATIONAL) {
@@ -76,8 +76,8 @@ public class CachetQueueTaskDispatcher extends QueueTaskDispatcher {
                                 msgs.add(message);
                                 log.info(message);
                                 requiredResourcesMetric.setGateUpdatedTime(System.currentTimeMillis());
+                                requiredResourcesMetric.setGatingStatus(r.getStatusId());
                             }
-                            requiredResourcesMetric.setGatingStatus(r.getStatusName());
                         }
                         metricsMap.put(name, requiredResourcesMetric);
                     }
